3831294633f3176b2728eb852e6aba4ddacd380a
Gregory-Hub <novikov.gri@gmail.com>
2024-02-20 17:05:33 +0300

    lab1

---
+++ Optimization Methods/lab1.py
@@ -0,0 +1,42 @@
+import numpy as np
+def f(x): return 2 * (x ** 2) - 9 * x - 31
+def df(x): return 4 * x - 9
+
+def bsearch(interval, tol):
+# searches for minimum using bisection method
+# arguments: bisectionsearch(f,df,interval,tol)
+# f - an objective function
+# df -  an objective function derivative
+# interval = [a, b] - search interval
+#tol - tolerance for both range and function value
+# output: [xmin, fmin, neval, coords]
+# xmin - value of x in fmin
+# fmin - minimul value of f
+# neval - number of function evaluations
+# coords - array of x values found during optimization
+
+    max_k = 100000
+
+    a, b = interval
+
+    coords = []
+    neval = 0
+
+    for i in range(max_k):
+        if (b - a) < tol:
+            break
+
+        x = (a + b) / 2
+        if df(x) > 0:
+            b = x
+        else:
+            a = x
+
+        neval += 1
+        coords.append(x)
+
+    xmin = x
+    fmin = f(x)
+
+    answer_ = [xmin, fmin, neval, coords]
+    return answer_
