622e032fabc8831705fad8fc1dc08699dcb244a2
Gregory-Hub <novikov.gri@gmail.com>
2024-03-03 15:35:42 +0300

    lab1

---
+++ Computer Graphics/lab1/lab1_compgr/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace lab1_compgr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(278, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(666, 557);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(128, 134);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 167);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 167);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 170);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(128, 218);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(143, 257);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 257);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(182, 254);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(49, 254);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(76, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(83, 302);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(122, 299);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(76, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(64, 410);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 63);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 577);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Computer Graphics/lab1/lab1_compgr/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_compgr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double x_point, y_point;
+        double x_circle, y_circle;
+        bool isFirstClick = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Точка";
+            label2.Text = "x";
+            label3.Text = "y";
+            label4.Text = "Окружность";
+            label5.Text = "y";
+            label6.Text = "x";
+            label7.Text = "Радиус - ";
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).Invalidate();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            if (isFirstClick)
+            {
+                ClearCoords(g);
+                isFirstClick = false;
+
+                x_point = e.X;
+                y_point = e.Y;
+
+                textBox1.Text = x_point.ToString();
+                textBox2.Text = y_point.ToString();
+
+                SolidBrush redBrush = new SolidBrush(Color.Red);
+                DrawFatPoint(g, (int)x_point, (int)y_point);
+            }
+            else
+            {
+                isFirstClick = true;
+
+                x_circle = e.X;
+                y_circle = e.Y;
+
+                textBox4.Text = x_circle.ToString();
+                textBox3.Text = y_circle.ToString();
+
+                DrawFatPoint(g, (int)x_circle, (int)y_circle);
+            }
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            //int width = ((PictureBox)sender).Width;
+            //int height = ((PictureBox)sender).Height;
+
+            //g.DrawLine(Pens.Black, 0, height / 2, width, height / 2); //x
+            //g.DrawLine(Pens.Black, width / 2, 0, width / 2, height); //y
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            if (!FormFilled())
+            {                
+                ClearCoords(g);
+                return;
+            }
+            
+            int radius = Convert.ToInt32(textBox5.Text);
+
+            Pen p = new Pen(Color.Red);
+
+            g.DrawEllipse(p, (int)x_circle - radius, (int)y_circle - radius, radius * 2, radius * 2);
+
+            double k = (y_circle - y_point) / (x_circle - x_point);
+            double m = -(k * x_point - y_point);
+            double hypotenuse = Math.Sqrt(Math.Pow(x_circle - x_point, 2) + Math.Pow(y_circle - y_point, 2));
+            double cos_alpha = radius / hypotenuse;
+            double angle_alpha = Math.Acos(cos_alpha);// * (180 / Math.PI);
+            double sin_alpha = Math.Sin(angle_alpha);
+
+            // x^2 + k^2*x^2 - 2x(xb) + 2kmx - 2kx(yb) + (xb)^2 + m^2 + (yb)^2 - 2m(yb)
+            // (1+k^2)x^2   +  (-2(xb)+2km-2k(yb))x   +   (xb)^2 + m^2 + (yb)^2 - 2m(yb) - r^2
+            double a = 1 + Math.Pow(k, 2);
+            double b = -2 * x_circle + (2 * k * m) - (2 * k * y_circle);
+            double c = Math.Pow(x_circle, 2) + Math.Pow(m, 2) + Math.Pow(y_circle, 2) - (2 * m * y_circle) - Math.Pow(radius, 2);
+            double D = Math.Pow(b, 2) - 4 * a * c;
+
+            double x_1 = (-b + Math.Sqrt(D)) / (2 * a);
+            double x_2 = (-b - Math.Sqrt(D)) / (2 * a);
+
+            double y_1 = k * x_1 + m;
+            double y_2 = k * x_2 + m;
+
+            double dist_1 = Math.Sqrt(Math.Pow(x_1 - x_point, 2) + Math.Pow(y_1 - y_point, 2));
+            double dist_2 = Math.Sqrt(Math.Pow(x_2 - x_point, 2) + Math.Pow(y_2 - y_point, 2));
+
+            double x_between_point, y_between_point;
+            if (dist_1 < dist_2)
+            {
+                x_between_point = x_1;
+                y_between_point = y_1;
+            }
+            else
+            {
+                x_between_point = x_2;
+                y_between_point = y_2;
+            }
+
+            // move to center, rotate around center and move back
+            double x_centralized = x_between_point - x_circle;
+            double y_centralized = y_between_point - y_circle;
+            double x_tangent_1 = x_centralized * cos_alpha - y_centralized * sin_alpha;
+            double y_tangent_1 = x_centralized * sin_alpha + y_centralized * cos_alpha;
+
+            double x_tangent_2 = x_centralized * cos_alpha - y_centralized * -sin_alpha;
+            double y_tangent_2 = x_centralized * -sin_alpha + y_centralized * cos_alpha;
+
+            x_tangent_1 += x_circle;
+            y_tangent_1 += y_circle;
+
+            x_tangent_2 += x_circle;
+            y_tangent_2 += y_circle;
+
+            DrawFatPoint(g, (int)x_tangent_1, (int)y_tangent_1);
+            DrawFatPoint(g, (int)x_tangent_2, (int)y_tangent_2);
+            g.DrawLine(p, new Point((int)x_point, (int)y_point), new Point((int)x_tangent_1, (int)y_tangent_1));
+            g.DrawLine(p, new Point((int)x_point, (int)y_point), new Point((int)x_tangent_2, (int)y_tangent_2));
+        }
+
+        private bool FormFilled()
+        {
+            return textBox1.Text != "" &&
+                textBox2.Text != "" &&
+                textBox3.Text != "" &&
+                textBox4.Text != "" &&
+                textBox5.Text != "";
+        }
+
+        private void ClearCoords(Graphics g)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            g.Clear(Color.White);
+            isFirstClick = true;
+        }
+
+        private void DrawFatPoint(Graphics g, int x, int y)
+        {
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            int fatness = 6;
+            g.FillRectangle(redBrush, x - fatness / 2, y - fatness / 2, fatness, fatness);
+        }
+    }
+}
